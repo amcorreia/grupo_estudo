4b10bd3a898a9a175955a8fb518307fa9f82228a
Julian <lashersmail@gmail.com>
2022-10-01 10:06:06 -0300

    maior

---
+++ codigos_teste/julian_221001_maiormenor.py
@@ -0,0 +1,6 @@
+numero1 = int(input("Digite um numero:"))
+numero2 = int(input("Digite um numero:"))
+if numero1 > numero2:
+    print("o numero 1 eh maior")
+else:
+    print("o numero 2 eh maior4")
