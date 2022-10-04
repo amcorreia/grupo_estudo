aa4ee324471c4a138cecf58d9c4878d68fd1c6f0
Grégori Santos <gregori.3@outlook.com.br>
2022-10-04 10:40:05 -0300

    notas

--- codigos_teste/gregori_221004_notas.py
+++ codigos_teste/gregori_221004_notas.py
@@ -3,14 +3,14 @@ n2 = int(input("Segunda nota: "))
 n3 = int(input("Terceira nota: "))
 n4 = int(input("Quarta nota: "))
 
-media = n1 + n2 + n3 + n4 / 2
+media = n1 + n2 + n3 + n4 / 4
 
 print("Notas:", n1, n2, n3, n4)
 
 print("Média:",media)
 
-if media >= 210:       # 60 + 60 + 60 + 60/2 = 210
+if media >= 195:       # 60 + 60 + 60 + 60/4 = 195
     print("Aprovado")
 
-if media < 210:
+if media < 195:
     print("Reprovado")    
