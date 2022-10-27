8aa5d2aa8351e0ea8b14c56fc5bb438def0cb270
Grégori Santos <gregori.3@outlook.com.br>
2022-10-27 09:51:52 -0300

    dois_metodos

---
+++ codigos_teste/gregori_221027_2metodos.py
@@ -0,0 +1,15 @@
+class Dois_metodos:                                            
+
+    def __init__(self, get_string):                              
+        self.get_string = input("Digite algo: ")
+
+
+    def print(self, print_string):                               
+        print(print_string)
+
+    pass
+
+A = Dois_metodos
+A = print("print_string")
+
+print(A)
