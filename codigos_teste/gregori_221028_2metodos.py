bd756c647b146a1d39ef7d9db2f38cbe7f52e229
Gregori <108099876+Gregoric@users.noreply.github.com>
2022-10-30 15:59:08 -0300

    Delete gregori_221028_2metodos.py

--- codigos_teste/gregori_221028_2metodos.py
+++
@@ -1,9 +0,0 @@
-class DoisMetodos():                                         
-
-    def __init__(self):                              
-        self.get_string = input("Digite algo: ")
-    
-metodo1 = DoisMetodos()
-
-
-print(metodo1.get_string)
